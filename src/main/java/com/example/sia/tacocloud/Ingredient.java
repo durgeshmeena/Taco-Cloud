88033a70e9bc4c865af6c83b94b2b088fc9e40b0
durgeshmeena <durgeshkumar@iitg.ac.in>
2022-12-13 15:35:53 +0530

    adding ingredient using lombok dependency

---
+++ src/main/java/com/example/sia/tacocloud/Ingredient.java
@@ -0,0 +1,18 @@
+package com.example.sia.tacocloud;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Ingredient {
+    
+    private final String id;
+    private final String name;
+    private final String type;
+
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+
+}
