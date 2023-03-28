8c027734c672e7bbb33c608fbe56d376667f22d8
durgeshmeena <durgeshkumar@iitg.ac.in>
2023-03-28 21:14:54 +0530

    adding Browser Tests

--- src/test/java/taco/tacocloud/HomeControllerTests.java
+++ src/test/java/taco/tacocloud/HomeControllerTests.java
@@ -8,11 +8,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 
-@SpringBootTest
+@WebMvcTest
 public class HomeControllerTests {
 
     @Autowired
